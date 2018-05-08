7edb21c1f963f748c859f7bc72022eafaf842ac1
Pål Andreassen <pal.andreassen@bouvet.no>
2018-05-08 22:17:54 +0200

    Initial commit

---
+++ Tool.SmtpTesterCore/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Mail;
+using System.Text;
+
+namespace Tool.SmtpTesterCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var smtpClient = new SmtpClient()
+            {
+                Host = "localhost",
+                Port = 25,
+                EnableSsl = false
+            };
+
+            var mailmessage = new MailMessage()
+            {
+                IsBodyHtml = true,
+                SubjectEncoding = Encoding.UTF8,
+                BodyEncoding = Encoding.UTF8,
+            };
+
+            mailmessage.From = new MailAddress("someone@here.com", "someone@here.com");
+            mailmessage.To.Add("someone@somewhere.com");
+            mailmessage.Body = "<b>This is a test</b><p>This is a test of outgoing emails with special characters in subject</p>";
+            mailmessage.Subject = "Async: Noe med æøå eller ÆØÅ og ß";
+
+            smtpClient.SendMailAsync(mailmessage).Wait();
+
+            mailmessage.Subject = "Sync: Noe med æøå eller ÆØÅ og ß";
+
+            smtpClient.Send(mailmessage);
+        }
+    }
+}
